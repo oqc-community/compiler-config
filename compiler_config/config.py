fd7e8d98a1960ced5f3859f1d4551e91e0166446
harry-oqc <hwaring@oxfordquantumcircuits.com>
2025-02-18 14:57:13 +0000

    Update compiler config deafult results_format to  binary_count

--- compiler_config/config.py
+++ compiler_config/config.py
@@ -227,7 +227,7 @@ class CompilerConfig:
     ):
         self.repeats: Optional[int] = repeats
         self.repetition_period: Optional[float] = repetition_period
-        self.results_format: QuantumResultsFormat = results_format or QuantumResultsFormat()
+        self.results_format: QuantumResultsFormat = results_format or QuantumResultsFormat().binary_count()
         self.metrics: MetricsType = metrics
         self.active_calibrations: List[CalibrationArguments] = active_calibrations or []
         self.optimizations: Optional[OptimizationConfig] = optimizations
