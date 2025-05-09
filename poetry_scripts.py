0e3052c0473568c5404127ec96852d429c266f49
Kajsa Eriksson Rosenqvist <keriksson.rosenqvist@oxfordquantumcircuits.com>
2025-05-09 10:39:31 +0100

    Ruff tooling setup

--- poetry_scripts.py
+++ poetry_scripts.py
@@ -2,6 +2,5 @@ import os
 
 
 def format_code():
-    os.system("poetry run black .")
-    os.system("poetry run isort .")
-    os.system("poetry run autoflake .")
+    os.system("poetry run ruff check --fix")
+    os.system("poetry run ruff format")
